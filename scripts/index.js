642743d73c5461680b7527cebeadbdf3eb5b8ee8
porotogus <gustavocarrena@outlook.com>
2022-03-20 19:51:14 -0300

    domingo

--- scripts/index.js
+++ scripts/index.js
@@ -5,15 +5,15 @@ headerSection()
 footerSection()
 
 $(document).ready(function(){
-    $('#leftBookcase').plaxify({"xRange":25,"yRange":25,"data-invert":false})
-    $('#calendar').plaxify({"xRange":15,"yRange":10,"data-invert":true})
-    $('#watch').plaxify({"xRange":10,"yRange":10,"data-invert":true})
+    $('#leftBookcase').plaxify({"xRange":25,"yRange":0,"data-invert":false})
+    $('#calendar').plaxify({"xRange":15,"yRange":0,"data-invert":true})
+    $('#watch').plaxify({"xRange":10,"yRange":0,"data-invert":true})
     $('#desk').plaxify({"xRange":100,"yRange":0,"data-invert":true})
     $('#screen').plaxify({"xRange":-20,"yRange":0,"data-invert":true})
-    $('#hello_msg').plaxify({"xRange":-26,"yRange":20})
+    $('#hello_msg').plaxify({"xRange":-26,"yRange":0})
     $('#apple').plaxify({"xRange":110,"yRange":0,"data-invert":false})
     $('#lamp').plaxify({"xRange":115,"yRange":0,"data-invert":false})
-    $('#rightBookcase').plaxify({"xRange":25,"yRange":25,"data-invert":false})
+    $('#rightBookcase').plaxify({"xRange":25,"yRange":0,"data-invert":false})
 
     $.plax.enable()
     });
